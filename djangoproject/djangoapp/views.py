1932ce2d42d6b3110248bc3dd1c0fa67184c39fa
agnkazimierska <agnieszka.kazimierska@pwr.edu.pl>
2024-11-15 21:18:03 +0100

    Problem set 6 (Django REST API) - Part 1 step 11 (plus modified migrations due to previous error)

--- djangoproject/djangoapp/views.py
+++ djangoproject/djangoapp/views.py
@@ -1,6 +1,7 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ValidationError
 from .models import Product
 from decimal import Decimal
 
@@ -15,31 +16,43 @@ def product_list(request):
 
     elif request.method == 'POST':
         data = json.loads(request.body)
+
+        if 'name' not in data or 'price' not in data or 'available' not in data:
+            return HttpResponseBadRequest('Missing required fields.')
+
         name = data.get('name')
         price = data.get('price')
         available = data.get('available')
 
-        product = Product(name=name, price=Decimal(str(price)), available=available)
-        product.full_clean()
-        product.save()
+        try:
+            product = Product(name=name, price=Decimal(str(price)), available=available)
+            product.full_clean()
+            product.save()
 
-        return JsonResponse({
-            'id': product.id,
-            'name': product.name,
-            'price': float(product.price),
-            'available': product.available
-            },
-            status=201
-        )
+            return JsonResponse({
+                'id': product.id,
+                'name': product.name,
+                'price': float(product.price),
+                'available': product.available
+                },
+                status=201
+            )
+        except ValidationError:
+            return HttpResponseBadRequest('Invalid data.')
+    return HttpResponseBadRequest(f'Method {request.method} not allowed on this endpoint.')
 
 @csrf_exempt
 def product_detail(request, product_id):
-    if request.method == 'GET':
-        product = Product.objects.get(id=product_id)
-        return JsonResponse({
-            'id': product.id,
-            'name': product.name,
-            'price': float(product.price),
-            'available': product.available
-            }
-        )
+    try:
+        if request.method == 'GET':
+            product = Product.objects.get(id=product_id)
+            return JsonResponse({
+                'id': product.id,
+                'name': product.name,
+                'price': float(product.price),
+                'available': product.available
+                }
+            )
+    except Product.DoesNotExist:
+        return HttpResponseNotFound(f'Product {product_id} not found.')
+    return HttpResponseBadRequest(f'Method {request.method} not allowed on this endpoint.')
