988465dc5938f8517872c9bf2f057bee4dc38d00
agnkazimierska <agnieszka.kazimierska@pwr.edu.pl>
2024-11-15 23:52:47 +0100

    Problem set 6 (Django MVT) - Part 3 step 7

--- djangoproject/djangoapp/urls.py
+++ djangoproject/djangoapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CustomerViewSet, OrderViewSet
 
+from .views import ProductListView, ProductDetailView, ProductCreateView
+
 # urlpatterns = [
 #     path('hello/', hello_world, name='hello_world'),
 # ]
@@ -11,7 +13,9 @@ router.register(r'products', ProductViewSet, basename='product')
 router.register(r'customers', CustomerViewSet, basename='customer')
 router.register(r'orders', OrderViewSet, basename='order')
 
-
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('user/products/', ProductListView.as_view(), name='product_list'),
+    path('user/products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('user/products/new/', ProductCreateView.as_view(), name='product_create'),
 ]
--- djangoproject/djangoapp/views.py
+++ djangoproject/djangoapp/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from .models import Product, Customer, Order
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
 
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import ProductForm
+
 # def hello_world(request):
 #     return HttpResponse("Hello, World!")
 
@@ -16,3 +20,24 @@ class CustomerViewSet(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name ='products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_create.html'
+    success_url = '../../products/'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
